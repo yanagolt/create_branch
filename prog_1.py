f683541a51259ec5f70d9061181af509610713a8
yanagolt <zvizdajared@gmail.com>
2019-02-08 20:12:07 +0200

    new_unit test

--- prog_1.py
+++ prog_1.py
@@ -1,3 +1,18 @@
+class Icecream():
+    def __init__(self, cream, wafel, jam):
+        self.cream = cream
+        self.wafel = True
+        self.jam = jam
+
+
+vanila = Icecream(cream="vanila", wafel=False, jam="strawberry")
+chocolate = Icecream(cream="chocolate", wafel=False, jam="pineapple")
+ice_juice = Icecream(cream="juice", wafel=False, jam="blackberry")
+
+
+
+
+
 
 
 
